cbe08c4df74bb4d033b074c17a6b3e472029d75e
Kristea Gamel <krgamel@gmail.com>
2019-03-23 12:37:41 -0500

    refactored Content to styled component

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-
+import styled from 'styled-components';
 import Tabs from './Tabs';
 import Cards from './Cards';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
 
+
+const ContentContainer = styled.div `
+  display: flex; 
+  flex-direction: column; 
+  align-items: center;
+`;
 export default class Content extends Component {
   constructor(props) {
     super(props);
@@ -55,7 +61,7 @@ export default class Content extends Component {
 
   render() {
     return (
-      <div className="content-container">
+      <ContentContainer>
         {/* 
           Add 2 props to the Tabs component, 
           `selectedTab` that includes the currently selected tab
@@ -67,7 +73,7 @@ export default class Content extends Component {
           selectTabHandler={this.changeSelected}
          />
         <Cards cards={this.filterCards()} />
-      </div>
+      </ContentContainer>
     );
   }
 }
